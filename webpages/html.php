1dcdddf20b66978fadb0a653201d0a06fcc68246
Brandyn Koehler <brandynkoehler@gmail.com>
2024-07-03 17:43:17 -0400

    Updated html.php - color mistype

--- webpages/html.php
+++ webpages/html.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles.css" />
   </head>
-  <body bgcolor="color:black">
+  <body bgcolor="black">
     <div id="container">
       <pre>
         <code>
