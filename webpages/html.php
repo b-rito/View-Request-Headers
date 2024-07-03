a364183525cc661592dc4c878a43dfa5f93383e9
Brandyn Koehler <brandynkoehler@gmail.com>
2024-07-03 17:29:11 -0400

    Update bg-color

--- webpages/html.php
+++ webpages/html.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles.css" />
   </head>
-  <body bgcolor="color:#100b14">
+  <body bgcolor="color:black">
     <div id="container">
       <pre>
         <code>
